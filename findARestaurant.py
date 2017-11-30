c12701d91db586ee9e7c2381348694ae876b68d3
Aman Gaur <aman.gaur1011@gmail.com>
2017-11-30 18:34:16 +0530

    solution code
    solution code

--- findARestaurant.py
+++ findARestaurant.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from geo_code import getGeoCodeLocation
 import json
 import httplib2
 
@@ -8,68 +7,69 @@ import codecs
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
-foursquare_client_id = "PASTE_YOUR_ID_HERE"
-foursquare_client_secret = "YOUR_SECRET_HERE"
+foursquare_client_id = 'KXTPUTXWJXUUUE22RHHQ3YNYEGZHBG31AXS0CFOHWL3AHANU'
+foursquare_client_secret = '3IF050LBAUYMCD1UV55HV2IAA1WOLR3NFMWYOAVYUVCNU5U2'
+google_api_key = 'AIzaSyBrLuHzG2mpXOAFwVefaA0797WMEgJYVHo'
 
+def getGeocodeLocation(inputString):
+    #Replace Spaces with '+' in URL
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+    h = httplib2.Http()
+    result = json.loads(h.request(url,'GET')[1])
+    #print response
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return (latitude,longitude)
 
-def findARestaurant(mealType,location):
-	#1. Use getGeocodeLocation to get the latitude and longitude coordinates of the location string.
-	latitude, longitude = getGeoCodeLocation(location)
+#This function takes in a string representation of a location and cuisine type, geocodes the location, and then pass in the latitude and longitude coordinates to the Foursquare API
+def findARestaurant(mealType, location):
+    latitude, longitude = getGeocodeLocation(location)
+    url = ('https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s' % (foursquare_client_id, foursquare_client_secret,latitude,longitude,mealType))
+    h = httplib2.Http()
+    result = json.loads(h.request(url,'GET')[1])
+    print result['response']
+    if result['response']['venues']:
+        #Grab the first restaurant
+        restaurant = result['response']['venues'][0]
+        venue_id = restaurant['id'] 
+        restaurant_name = restaurant['name']
+        restaurant_address = restaurant['location']['formattedAddress']
+        #Format the Restaurant Address into one string
+        address = ""
+        for i in restaurant_address:
+            address += i + " "
+        restaurant_address = address
+        
+        #Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
+        url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&v=20150603&client_secret=%s' % ((venue_id,foursquare_client_id,foursquare_client_secret)))
+        result = json.loads(h.request(url,'GET')[1])
+        #Grab the first image
+        #if no image available, insert default image url
+        if result['response']['photos']['items']:
+            firstpic = result['response']['photos']['items'][0]
+            prefix = firstpic['prefix']
+            suffix = firstpic['suffix']
+            imageURL = prefix + "300x300" + suffix
+        else:
+            imageURL = "http://pixabay.com/get/8926af5eb597ca51ca4c/1433440765/cheeseburger-34314_1280.png?direct"
 
-	CLIENT_ID = 'KXTPUTXWJXUUUE22RHHQ3YNYEGZHBG31AXS0CFOHWL3AHANU'
-	CLIENT_SECRET = '3IF050LBAUYMCD1UV55HV2IAA1WOLR3NFMWYOAVYUVCNU5U2'
-	#2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
-	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
-	url = ('https://api.foursquare.com/v2/venues/search?ll=%s,%s&client_id=%s&client_secret=%s&v=20171127&intent=browser&radius=20000&query=%s'% (latitude, longitude, CLIENT_ID, CLIENT_SECRET, mealType))
-	h = httplib2.Http()
-	response, content = h.request(url, 'GET')
-
-	result = json.loads(content)
-
-	#3. Grab the first restaurant
-	venue = result['response']['venues'][0]
-
-	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
-	url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&client_secret=%s&v=20171127'% (venue['id'], CLIENT_ID, CLIENT_SECRET))
-	response, content = h.request(url, 'GET')
-	result = json.loads(content)
-
-	#5. Grab the first image
-	if result['response']['photos']['items']:
-		photo = result['response']['photos']['items'][0]
-		photourl = photo['prefix'] + '300x300' + photo['suffix']
-	#6. If no image is available, insert default a image url
-	else:
-		photourl = 'http://www.ramw.org/sites/default/files/styles/news_item/public/default_images/default_0.jpg?itok=t5AdiGCF'
-
-	#7. Return a dictionary containing the restaurant name, address, and image url	
-	
-
-	address = ''
-	print venue['location']['formattedAddress']
-	fullAddress = venue['location']['formattedAddress']
-	print fullAddress[0]
-	for i in fullAddress:
-		print i
-		address += i + ' '
-	print address
-
-	dictionary = {'Restaurant Name': venue['name'], 'Restaurant Address': address,'Image': photourl}
-
-	print "Restaurant Name: %s" % dictionary['Restaurant Name']
-	print "Restaurant Address: %s" % dictionary['Restaurant Address']
-	print "Image: %s" % dictionary['Image']
-	print "\n"
-
-	return dictionary
-
+        restaurantInfo = {'name':restaurant_name, 'address':restaurant_address, 'image':imageURL}
+        #print "Restaurant Name: %s " % restaurantInfo['name']
+        #print "Restaurant Address: %s " % restaurantInfo['address']
+        #print "Image: %s \n " % restaurantInfo['image']
+        return restaurantInfo
+    else:
+        #print "No Restaurants Found for %s" % location
+        return "No Restaurants Found"
+    
 if __name__ == '__main__':
-	findARestaurant("Pizza", "Tokyo, Japan")
-	findARestaurant("Tacos", "Jakarta, Indonesia")
-	findARestaurant("Tapas", "Maputo, Mozambique")
-	findARestaurant("Falafel", "Cairo, Egypt")
-	findARestaurant("Spaghetti", "New Delhi, India")
-	findARestaurant("Cappuccino", "Geneva, Switzerland")
-	findARestaurant("Sushi", "Los Angeles, California")
-	findARestaurant("Steak", "La Paz, Bolivia")
-	findARestaurant("Gyros", "Sydney Australia")
+    findARestaurant("Pizza", "Tokyo, Japan")
+    findARestaurant("Tacos", "Jakarta, Indonesia")
+    findARestaurant("Tapas", "Maputo, Mozambique")
+    findARestaurant("Falafel", "Cairo, Egypt")
+    findARestaurant("Spaghetti", "New Delhi, India")
+    findARestaurant("Cappuccino", "Geneva, Switzerland") 
+    findARestaurant("Sushi", "Los Angeles, California")
+    findARestaurant("Steak", "La Paz, Bolivia")
+    findARestaurant("Gyros", "Sydney Austrailia")
--- views.py
+++ views.py
@@ -1,4 +1,3 @@
-
 from findARestaurant import findARestaurant
 from models import Base, Restaurant
 from flask import Flask, jsonify, request
@@ -14,11 +13,11 @@ sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
 
 
-foursquare_client_id = 'KXTPUTXWJXUUUE22RHHQ3YNYEGZHBG31AXS0CFOHWL3AHANU'
+#foursquare_client_id = ''
 
-foursquare_client_secret = '3IF050LBAUYMCD1UV55HV2IAA1WOLR3NFMWYOAVYUVCNU5U2'
+#foursquare_client_secret = ''
 
-google_api_key = 'AIzaSyBrLuHzG2mpXOAFwVefaA0797WMEgJYVHo'
+#google_api_key = ''
 
 engine = create_engine('sqlite:///restaruants.db')
 
@@ -29,58 +28,51 @@ app = Flask(__name__)
 
 @app.route('/restaurants', methods = ['GET', 'POST'])
 def all_restaurants_handler():
-    #YOUR CODE HERE
-    if request.method == 'POST':
-        print "reached in post"
-        location = request.args.get('location', '')
-        mealType = request.args.get('mealType', '')
+  if request.method == 'GET':
+    # RETURN ALL RESTAURANTS IN DATABASE
+    restaurants = session.query(Restaurant).all()
+    return jsonify(restaurants = [i.serialize for i in restaurants])
 
-        restaurant = findARestaurant(mealType, location)
-        print restaurant
-        if restaurant:
-            addedRestaurant = Restaurant(restaurant_name=restaurant['Restaurant Name'], restaurant_address=restaurant['Restaurant Address'], restaurant_image=restaurant['Image'])
-            session.add(addedRestaurant)
-            session.commit()
-
-            print "reached before returning"
-            return jsonify(restaurants = addedRestaurant.serialize)
-        else:
-            return "no restaurant found"
-
+  elif request.method == 'POST':
+    # MAKE A NEW RESTAURANT AND STORE IT IN DATABASE
+    location = request.args.get('location', '')
+    mealType = request.args.get('mealType', '')
+    restaurant_info = findARestaurant(mealType, location)
+    if restaurant_info != "No Restaurants Found":
+      restaurant = Restaurant(restaurant_name = unicode(restaurant_info['name']), restaurant_address = unicode(restaurant_info['address']), restaurant_image = restaurant_info['image'])
+      session.add(restaurant)
+      session.commit() 
+      return jsonify(restaurant = restaurant.serialize)
     else:
-        allRestaurants = session.query(Restaurant).all()
-        return jsonify(restaurants = [restaurant.serialize for restaurant in allRestaurants])
+      return jsonify({"error":"No Restaurants Found for %s in %s" % (mealType, location)})
     
 @app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
 def restaurant_handler(id):
-    #YOUR CODE HERE
-    restaurant = session.query(Restaurant).filter_by(id=id).one()
+  restaurant = session.query(Restaurant).filter_by(id = id).one()
+  if request.method == 'GET':
+    #RETURN A SPECIFIC RESTAURANT
+    return jsonify(restaurant = restaurant.serialize)
+  elif request.method == 'PUT':
+    #UPDATE A SPECIFIC RESTAURANT
+    address = request.args.get('address')
+    image = request.args.get('image')
+    name = request.args.get('name')
+    if address:
+        restaurant.restaurant_address = address
+    if image:
+        restaurant.restaurant_image = image
+    if name:
+        restaurant.restaurant_name = name
+    session.commit()
+    return jsonify(restaurant = restaurant.serialize)
 
-    if request.method == 'PUT':
-        if request.args.get('name'):
-            restaurant.restaurant_name = request.args.get('name')
-        if request.args.get('address'):
-            restaurant.restaurant_address = request.args.get('address')
-        if request.args.get('image'):
-            restaurant.restaurant_image = rrequest.args.get('image')
-
-        session.add()
-        session.commit()
-        return jsonify(restaurants = restaurant.serialize)
-
-    elif request.method == 'DELETE':
-        
-        session.delete(restaurant)
-        session.commit()
-        return "Restaurant deleted successfully"
-    else:
-        
-        return jsonify(restaurant = restaurant.serialize)
-
-
-
-
+  elif request.method == 'DELETE':
+    #DELETE A SPECFIC RESTAURANT
+    session.delete(restaurant)
+    session.commit()
+    return "Restaurant Deleted"
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
+
