49730af07e1eff3ff1f48fb5caaa0d1ca91b7b6a
Aman Gaur <aman.gaur1011@gmail.com>
2017-12-01 19:47:01 +0530

    making sure User works

--- project/view.py
+++ project/view.py
@@ -1,14 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from models import Base, Music_Band, Album, User
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 app = Flask(__name__)
 
 # login session.
-from flask import 
+from flask import session as login_session
+import random, string
+
+# imports for the google auth
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+CLIENT_ID_GOOGLE = json.loads(open('client_secret.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Music Album Catalog App"
+
+# connect to database
+engine = create_engine('sqlite:///musicbandswithalbums.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
 def homePage():
+	print CLIENT_ID_GOOGLE
+	session.add(User(username = 'Agaur'))
+	session.commit()
+	user = session.query(User).all()
+	print user[0].username
+	session.delete(user[0])
+	session.commit()
 	return render_template('main.html')
 
 
